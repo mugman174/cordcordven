91e38b624d1b1192b35dc3fe637a2bfa0cb0bc47
mugman174 <53713683+mugman174@users.noreply.github.com>
2023-10-09 07:43:22 -0700

    the script

---
+++ pack.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# most of this code from @SomeAspy
+
+#SECTION - SETUP
+
+cd ..
+git config --global pull.rebase false
+git clone https://github.com/vendicated/vencord
+git clone https://github.com/SomeAspy/ThirdPartyVencordPlugins --recurse-submodules
+
+#!SECTION
+#SECTION - Integrate 3rd-party plugins
+
+(cd ThirdPartyVencordPlugins || { echo "Filesystem Failure"; exit 1;}
+bash repack.sh)
+mkdir vencord/src/userplugins
+cp -Rf ThirdPartyVencordPlugins/dist/* vencord/src/userplugins/
+
+#!SECTION
+#SECTION - Build Vencord
+
+(cd vencord || { echo "Filesystem Failure"; exit 1;}
+pnpm i --frozen-lockfile
+pnpm run buildWeb)
+
+#!SECTION
+#SECTION - Deploy
+
+cp -Rf vencord/dist/* dist
+
+#!SECTION
+
+echo (originally) made by someaspy on Discord - https://github.com/SomeAspy - https://aspy.dev
